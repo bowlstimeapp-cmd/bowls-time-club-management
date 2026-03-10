ca8b9ec2c044e9760f2643a051f572fc0f380f4d
Bowls Time <bowlstimeapp@gmail.com>
2026-03-10 20:14:53 +0000

    Manual code change

--- src/pages/SelectionEditor.jsx
+++ src/pages/SelectionEditor.jsx
@@ -237,7 +237,7 @@ export default function SelectionEditor() {
       })
       .join('\n');
 
-    const matchUrl = `${APP_BASE_URL}${createPageUrl('SelectionView')}?clubId=${clubId}&selectionId=${savedSelectionId}`;
+    const matchUrl = `https://app.bowls-time.com/FunctionRoomAdmin${createPageUrl('SelectionView')}?clubId=${clubId}&selectionId=${savedSelectionId}`;
     
 // Email notifications
 if (club?.email_member_notifications) {
